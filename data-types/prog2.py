8af9c0c56597cf7991fd3f8ec7c73a2a277ab15d
Cynefohub <146325296+Cynefohub@users.noreply.github.com>
2024-01-27 17:59:24 +0530

    added remove statement

--- data-types/prog2.py
+++ data-types/prog2.py
@@ -17,18 +17,25 @@ c.insert(len(c), "cynefohub")
 
 print(c)
 
+## Deletion of an element from the list
+c.remove("go")
+print(c)
+
+c.pop()
+print(c)
+
 ####-----------
 
 '''
 Tuple Data type
 '''
-a=(1,2,3,4)
-print(a) #prints the whole tuple
+# a=(1,2,3,4)
+# print(a) #prints the whole tuple
 
-#tuple having multiple type of data.
-b=("hello", 1,2,3,"go")
-print(b) #prints the whole tuple
+# #tuple having multiple type of data.
+# b=("hello", 1,2,3,"go")
+# print(b) #prints the whole tuple
 
-#index of tuples are also 0 based.
+# #index of tuples are also 0 based.
 
-print(b[4]) #this prints a single element in a tuple, in this case "go"
+# print(b[4]) #this prints a single element in a tuple, in this case "go"
